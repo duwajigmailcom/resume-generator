4adbf1abc490cd2c11b55c8b22ed5d7bac5643c4
marwan duwaji <duwaji@gmail.com>
2020-01-05 12:05:57 -0500

    moving state up one level

--- src/App.js
+++ src/App.js
@@ -1,37 +1,99 @@
-import React, { Component } from 'react';
-import { Tabs, Tab } from "@material-ui/core"
-import ContactInfo from "./components/contactInfo"
-import Education from "./components/education"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React, { Component, Fragment } from 'react';
+import { Tabs, Tab } from "@material-ui/core";
+import { withRouter } from "react-router-dom";
+import ContactInfo from "./components/contactInfo";
+import Education from "./components/education";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contactInfo: {
+        name: "",
+        address: "",
+        tel: "",
+        email: "",
+        linkedIn: ""
+      }
+    };
+    this.firstNames = ["Emma", "Olivia", "Ava", "Isabella", "Sophia", "Charlotte", "Mia", "Amelia", "Harper", "Evelyn"];
+    this.lastNames = ["Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson"];
+    this.cityNames = ["Little Falls", "Great Falls", "Spring Falls", "Hidden Falls", "High Falls", "Niagra Falls"];
+    this.stateNames = ["CA", "NC", "SC", "FL", "MA", "NH", "NB", "WA", "AZ", "IL", "OH"];
+    this.zipCodes = ["87654", "44567", "98875", "34534", "34523", "34567", "16534", "23434", "45673", "23456", "23423"];
+    this.telNumbers = ["453 345 3453", "564 343 4553", "768 345 4564", "976 645 2334"];
+  };
+
+  fillContact = () => {
+    const randomFirstName = this.firstNames[this.getRandomIndex(this.firstNames.length)];
+    const randomLastName = this.lastNames[this.getRandomIndex(this.lastNames.length)];
+    const randomCityName = this.cityNames[this.getRandomIndex(this.cityNames.length)];
+    const randomStateName = this.stateNames[this.getRandomIndex(this.stateNames.length)];
+    const randomZipCode = this.zipCodes[this.getRandomIndex(this.zipCodes.length)];
+    const randomTelNumber = this.telNumbers[this.getRandomIndex(this.telNumbers.length)];
+    this.setState({ contactInfo: {
+      name: `${randomFirstName} ${randomLastName}`,
+      address: `${randomCityName}, ${randomStateName} ${randomZipCode}`,
+      tel: `${randomTelNumber}`,
+      email: `${randomFirstName.toLowerCase()}.${randomLastName.toLowerCase()}@gmail.com`,
+      linkedIn: `https://www.linkedin.com/in/${randomFirstName.toLowerCase()}${randomLastName.toLowerCase()}`
+      }
+    });
+  };
+
+  clearContact = () => {
+    this.setState({
+      contactInfo: {
+        name: "",
+        address: "",
+        tel: "",
+        email: "",
+        linkedIn: ""
+      }
+    });
+  };
+
+  getRandomIndex = max => {
+    const randomIndex = Math.floor(Math.random() * max);
+    if (randomIndex < 0 || randomIndex > max)
+      console.error("Error: invalid index");
+    return randomIndex;
+  };
+
+  nextStep = () => {
+    console.log("next");
+  };
+
+  onTabChange = (event, newValue) => {
+    this.props.history.push(newValue);
+  }
+
   render() {
+    const { history } = this.props;
+    const path = history && history.location && history.location.pathname;
     return (
-      <Router>
-        <Tabs>
-          <Link to="/"><Tab label="Contact Info"></Tab></Link>
-          <Link to="/education"><Tab label="Education"></Tab></Link>
-          <Link to="/experience"><Tab label="Experience"></Tab></Link>
+      <Fragment>
+        <Tabs value={path} onChange={this.onTabChange}>
+          <Tab label="Contact Info" value="/" />
+          <Tab label="Education" value="/education" />
+          <Tab label="Experience" value="/experience" />
         </Tabs>
-        <div style={{ padding: 24 }}>
-          <Switch>
-            <Route path="/education">
-              <Education />
-            </Route>
-            <Route path="/experience">
-              <Experience />
-            </Route>
-            <Route path="/">
-              <ContactInfo />
-            </Route>
-          </Switch>
+        <div style={{padding: 24}}>
+          {path === "/" &&
+            <ContactInfo
+              values={this.state.contactInfo}
+              fillContactHandler={this.fillContact}
+              clearContactHandler={this.clearContact}
+              nextHandler={this.nextStep} />
+          }
+          {path === "/education" &&
+            <Education />
+          }
+          {path === "/experience" &&
+            <Experience />
+          }
         </div>
-      </Router>
+      </Fragment>
     )
   }
 }
@@ -40,4 +102,4 @@ function Experience() {
   return <h2>Experience</h2>;
 }
 
-export default App;
+export default withRouter(App);
--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -1,83 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import { Grid, TextField, Button } from "@material-ui/core";
 
-class ContactInfo extends Component {
-    constructor() {
-        super();
-        this.state = {
-            name: "",
-            address: "",
-            tel: "",
-            email: "",
-            linkedIn: ""
-        };
-        this.firstNames = ["Emma", "Olivia", "Ava", "Isabella", "Sophia", "Charlotte", "Mia", "Amelia", "Harper", "Evelyn"];
-        this.lastNames = ["Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson"];
-        this.cityNames = ["Little Falls", "Great Falls", "Spring Falls", "Hidden Falls", "High Falls", "Niagra Falls"];
-        this.stateNames = ["CA", "NC", "SC", "FL", "MA", "NH", "NB", "WA", "AZ", "IL", "OH"];
-        this.zipCodes = ["87654", "44567", "98875", "34534", "34523", "34567", "16534", "23434", "45673", "23456", "23423"];
-        this.telNumbers = ["453 345 3453", "564 343 4553", "768 345 4564", "976 645 2334"];
-    };
+const ContactInfo = props => {
 
-    autoGenerateContact = () => {
-        const randomFirstName = this.firstNames[this.getRandomIndex(this.firstNames.length)];
-        const randomLastName = this.lastNames[this.getRandomIndex(this.lastNames.length)];
-        const randomCityName = this.cityNames[this.getRandomIndex(this.cityNames.length)];
-        const randomStateName = this.stateNames[this.getRandomIndex(this.stateNames.length)];
-        const randomZipCode = this.zipCodes[this.getRandomIndex(this.zipCodes.length)];
-        const randomTelNumber = this.telNumbers[this.getRandomIndex(this.telNumbers.length)];
-        this.setState({
-            name: `${randomFirstName} ${randomLastName}`,
-            address: `${randomCityName}, ${randomStateName} ${randomZipCode}`,
-            tel: `${randomTelNumber}`,
-            email: `${randomFirstName.toLowerCase()}.${randomLastName.toLowerCase()}@gmail.com`,
-            linkedIn: `https://www.linkedin.com/in/${randomFirstName.toLowerCase()}${randomLastName.toLowerCase()}`
-        });
-    };
-
-    clear = () => {
-        this.setState({
-            name: "",
-            address: "",
-            tel: "",
-            email: "",
-            linkedIn: ""
-        });
-    };
-
-    getRandomIndex = max => {
-        const randomIndex = Math.floor(Math.random() * max);
-        if (randomIndex < 0 || randomIndex > max)
-            console.error("Error: invalid index");
-        return randomIndex;
-    };
-
-    render() {
-        return (
-            <Grid container spacing={2} style={{ textAlign: "center" }}>
-                <Grid item xs={12}>
-                    <TextField fullWidth label="Name" variant="outlined" value={this.state.name} />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField fullWidth label="City, State, and Zip" variant="outlined" value={this.state.address} />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField fullWidth label="Telephone" variant="outlined" value={this.state.tel} />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField fullWidth label="Email" variant="outlined" value={this.state.email} />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField fullWidth label="LinkedIn Profile" variant="outlined" value={this.state.linkedIn} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant="outlined" onClick={this.autoGenerateContact}>Auto Generate</Button> 
-                    {" "}<Button variant="outlined" color="secondary" onClick={this.clear}>Clear</Button>
-                    {" "}<Button variant="outlined" color="primary">Next</Button>
-                </Grid>
+    const { values, fillContactHandler, clearContactHandler, nextHandler } = props;
+    return (
+        <Grid container spacing={2} style={{ textAlign: "center" }}>
+            <Grid item xs={12}>
+                <TextField fullWidth label="Name" variant="outlined" value={values.name} />
             </Grid>
-        )
-    }
+            <Grid item xs={12}>
+                <TextField fullWidth label="City, State, and Zip" variant="outlined" value={values.address} />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField fullWidth label="Telephone" variant="outlined" value={values.tel} />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField fullWidth label="Email" variant="outlined" value={values.email} />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField fullWidth label="LinkedIn Profile" variant="outlined" value={values.linkedIn} />
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="outlined" onClick={fillContactHandler}>Auto Fill</Button>
+                {" "}<Button variant="outlined" color="secondary" onClick={clearContactHandler}>Clear</Button>
+                {" "}<Button variant="outlined" color="primary" onClick={nextHandler}>Next</Button>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default ContactInfo;
